5da997fb76a0545d49a910e06fc009ea4a0c176e
Daniel <danleavitt0@gmail.com>
2016-03-18 19:26:24 -0700

    positive and negative tests

--- test/test.js
+++ test/test.js
@@ -1,28 +1,47 @@
 var test = require('tape')
 var API = require('./testAPI')
 var WebSocket = require('ws')
-var ws = new WebSocket('ws://localhost:3000')
-var id = 1
 var server = require('../lib/index')(API, 3000)
+var ws = new WebSocket('ws://localhost:3000')
 
-setTimeout(function () {
-  server.close()
-  ws.close()
-}, 2000)
+test('realtime server positive response', function (t) {
+  var id = 1
 
-
-test('realtime server', function (t) {
-  t.plan(1)
+  t.plan(3)
   ws.on('open', function () {
     ws.send(JSON.stringify({
       type: 'ping',
-      id: id++
+      id: id
     }))
   })
   ws.on('message', function (data) {
     data = JSON.parse(data)
-    t.equals(id - 1, data.reply_id)
+    t.ok(data.reply_id)
+    t.ok(data.ok)
+    t.equals(id, data.reply_id)
+  })
+})
+
+test('realtime server negative response', function (t) {
+  server.close()
+  ws.close()
+  ws = new WebSocket('ws://localhost:3000')
+  server = require('../lib/index')(API, 3000)
+  var id = 2
+
+  t.plan(3)
+  ws.on('open', function () {
+    ws.send(JSON.stringify({
+      type: 'pong',
+      id: id
+    }))
+  })
+  ws.on('message', function (data) {
+    data = JSON.parse(data)
+    t.ok(data.reply_id)
+    t.notOk(data.ok)
+    t.equals(id, data.reply_id)
+    server.close()
     ws.close()
-    t.end()
   })
 })
