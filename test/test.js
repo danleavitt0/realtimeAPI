a14ea43919a351079f36ad9a70c797f688c60149
Daniel <danleavitt0@gmail.com>
2016-03-18 17:18:00 -0700

    return server

--- test/test.js
+++ test/test.js
@@ -3,19 +3,26 @@ var API = require('./testAPI')
 var WebSocket = require('ws')
 var ws = new WebSocket('ws://localhost:3000')
 var id = 1
-require('../lib/index')(API, 3000)
+var server = require('../lib/index')(API, 3000)
 
-ws.on('open', function () {
-  test('realtime server', function (t) {
-    t.plan(1)
+setTimeout(function () {
+  server.close()
+  ws.close()
+}, 2000)
+
+
+test('realtime server', function (t) {
+  t.plan(1)
+  ws.on('open', function () {
     ws.send(JSON.stringify({
       type: 'ping',
       id: id++
     }))
-    ws.on('message', function (data) {
-      data = JSON.parse(data)
-      t.equals(id - 1, data.reply_id)
-      t.end()
-    })
+  })
+  ws.on('message', function (data) {
+    data = JSON.parse(data)
+    t.equals(id - 1, data.reply_id)
+    ws.close()
+    t.end()
   })
 })
